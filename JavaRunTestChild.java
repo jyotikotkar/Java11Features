3a67847bb4be22f0bc4190f6237d6e1bb46d5aed
jyotikotkar <jyo.salunkhe19@gmail.com>
2021-03-21 15:53:53 +0100

    Add files via upload
    - Initial commit

---
+++ JavaRunTestChild.java
@@ -0,0 +1,9 @@
+package com.backbase.compilation;
+
+public class JavaRunTestChild extends JavaRunTest {
+
+    public static void main(String args[]) {
+
+        System.out.println("Inherited Class   "+ parentFiled );
+    }
+}
